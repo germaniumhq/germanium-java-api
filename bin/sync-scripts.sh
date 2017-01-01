84a2c1be2172d0e9583080559318d7de6a9da476
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-01-01 22:54:25 +0100

    Alphabetic ordering.

--- bin/sync-scripts.sh
+++ bin/sync-scripts.sh
@@ -7,14 +7,15 @@ GERMANIUM_PYTHON=$(readlink -f $(dirname $(readlink -f "$0"))/../../germanium/)
 
 
 mkdir -p $GERMANIUM_JAVA/src/main/resources/germanium/locators/
-mkdir -p $GERMANIUM_JAVA/src/main/resources/germanium/util/
 mkdir -p $GERMANIUM_JAVA/src/main/resources/germanium/points/
+mkdir -p $GERMANIUM_JAVA/src/main/resources/germanium/util/
 
 cp -R $GERMANIUM_PYTHON/germanium/locators/*.js \
       $GERMANIUM_JAVA/src/main/resources/germanium/locators/
 
+cp -R $GERMANIUM_PYTHON/germanium/points/*.js \
+      $GERMANIUM_JAVA/src/main/resources/germanium/points/
+
 cp -R $GERMANIUM_PYTHON/germanium/util/*.js \
       $GERMANIUM_JAVA/src/main/resources/germanium/util/
 
-cp -R $GERMANIUM_PYTHON/germanium/points/*.js \
-      $GERMANIUM_JAVA/src/main/resources/germanium/points/
